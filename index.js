b0a91f1df6034eebfa50a33bc46f691431c1de1b
miljakz <145482434+miljakz@users.noreply.github.com>
2024-07-20 12:21:59 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,24 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser');  
-const cors = require('cors');               
-const connectDB = require('./config/db');   
-require('dotenv').config();                 
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const connectDB = require('./config/db');
+require('dotenv').config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;      
+const PORT = process.env.PORT || 5000;
 
-// Connect to MongoDB
 connectDB();
 
-// Middleware
-app.use(cors());                            
-app.use(bodyParser.json());                 
+app.use(cors());
+app.use(bodyParser.json());
 
-// Routes
-app.use('/api/categories', require('./routes/categoryRoutes'));  
-app.use('/api/tasks', require('./routes/taskRoutes'));           
+app.use('/api/categories', require('./routes/categoryRoutes'));
+app.use('/api/tasks', require('./routes/taskRoutes'));
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
