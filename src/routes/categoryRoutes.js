176f768cd84598932d9b19ca2f1a2d5b4cefc6b8
miljakz <145482434+miljakz@users.noreply.github.com>
2024-07-22 17:45:17 +0200

    Update categoryRoutes.js

--- src/routes/categoryRoutes.js
+++ src/routes/categoryRoutes.js
@@ -5,18 +5,11 @@ const {
   createCategory,
   deleteCategory,
   updateCategory
-} = require('../controllers/categoryController');
+} = require('../src/controllers/categoryController'); 
 
-// Get all categories
 router.get('/', getCategories);
-
-// Create a new category
 router.post('/', createCategory);
-
-// Delete a category
 router.delete('/:id', deleteCategory);
-
-// Update a category
 router.put('/:id', updateCategory);
 
 module.exports = router;
