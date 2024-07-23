846e4cff4ccb57ea61d7a142b3714728034e04a1
miljakz <145482434+miljakz@users.noreply.github.com>
2024-07-23 11:25:31 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,27 +2,25 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const morgan = require('morgan');
-const helmetConfig = require('./helmetConfig'); // Import the helmetConfig
-const dotenv = require('dotenv');
+const helmet = require('helmet');
 const secureRoutes = require('./routes/secureRoutes'); 
 
 const app = express();
 
 // Environment configuration
-dotenv.config();
+require('dotenv').config();
 
 // Middleware
 app.use(cors());
-app.use(helmetConfig); 
-app.use(morgan('dev')); 
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true })); 
+app.use(helmet());
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-// Serve static files
 app.use(express.static('public'));
 
 // Routes
-app.use('/api', secureRoutes); // Secure routes
+app.use('/api', secureRoutes);
 
 // Welcome route
 app.get('/', (req, res) => {
@@ -34,9 +32,9 @@ app.use((req, res, next) => {
     res.status(404).send('Not Found');
 });
 
-// Basic error handler
+// Error handler
 app.use((err, req, res, next) => {
-    console.error(err.stack);
+    console.error('Internal Server Error:', err);
     res.status(500).send('Something broke!');
 });
 
