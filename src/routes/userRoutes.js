af953973c93dc8305632ff720e3fbd425820a09e
miljakz <145482434+miljakz@users.noreply.github.com>
2024-07-23 15:08:58 +0200

    Update userRoutes.js

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,9 +1,18 @@
 const express = require('express');
 const router = express.Router();
-const userController = require('../controllers/userController');
+const verifyToken = require('../middleware/auth');
+const User = require('../models/User');
 
-router.post('/user/:id/pomodoro/start', userController.startPomodoroSession);
-router.post('/user/:id/pomodoro/complete', userController.completePomodoroSession);
-router.put('/user/:id/pomodoro/settings', userController.updatePomodoroSettings);
+// Create a new user
+router.post('/users', verifyToken, async (req, res) => {
+  const { uid, email } = req.body;
+  try {
+    const newUser = new User({ uid, email });
+    await newUser.save();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 
 module.exports = router;
